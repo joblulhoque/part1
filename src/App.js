71138040a0dc9322ee7528600c18ee5837b25dc4
joblul <jhoque@maristak.net>
2022-03-14 17:20:24 +0100

    exercises 1.2 completed

--- src/App.js
+++ src/App.js
@@ -8,13 +8,37 @@ const Header = (props) => {
   )
 };
 
-const Content = (props) => {
+const Part1 = (props) => {
   return (
-    <>
-      <p>{props.part1} {props.exercises1}</p>
+    <div>
+       <p>{props.part1} {props.exercises1}</p>
+    </div>
+  )
+ }
+
+const Part2 = (props) => {
+  return (
+    <div>
       <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part2} {props.exercises3}</p>
-    </>
+    </div>
+  )
+}
+const Part3 = (props) => {
+  return (
+    <div>
+      <p>{props.part3} {props.exercises3}</p>
+    </div>
+  )
+}
+
+const Content = (props) => {
+
+  return (
+    <div>
+      <Part1 part1={props.part1} exercises1 = {props.exercises1}/>
+      <Part2 part2 = {props.part2} exercises2 = {props.exercises2}/>
+      <Part3 part3 = {props.part3} exercises3 = {props.exercises3}/>
+    </div>
   )
 }
 
@@ -30,13 +54,14 @@ const Total = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
+  const course = 'Half Stack application development';
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
   const part2 = 'Using props to pass data'
   const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part3 = 'State of a component';
+  const exercises3 = 14;
+  
 
   return (
     <div>
